b1697cc46a328609bb7a895ec0efc2804bd0b099
Amanda <amandaben21@aol.com>
2021-05-28 00:14:43 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+let num3 = 15
+let num4 = 11
 
-// Write your code here
+let multiply = num1 * num2 //is an equation whose multiplied results will equal 62
+
+let random = Math.floor(Math.random() * 10) + 1 //generates a random integer greater than 0
+
+let mod = num3 % num4 //is an equation that calculates a remainder that is equal to 4
+
+let max = Math.max(1,2,3,20) //will return 20 as the highest number in the set
